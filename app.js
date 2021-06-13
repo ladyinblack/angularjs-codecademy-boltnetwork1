ea21124da9dd4786fdd94d51436a555ddde021f8
ladyinblack <ladyinblack2010rs@gmail.com>
2021-06-13 22:03:51 +0200

    Updated home controller

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import angular from 'angular';
-// import '@uirouter/angularjs';
+import '@uirouter/angularjs';
 
 // Import your app stylesheets
 import './style.css';
 
 // Import your app functionality
-// import './home';
+import './home';
 
 // Create and bootstrap application
-// const requires = ['ui.router', 'home'];
+const requires = ['ui.router', 'home'];
 
-// window.app = angular.module('app', requires);
+window.app = angular.module('app', requires);
 
-// angular.bootstrap(document.getElementById('app'), ['app']);
+angular.bootstrap(document.getElementById('app'), ['app']);
